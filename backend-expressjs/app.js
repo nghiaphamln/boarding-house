08944e7f1fe47630ba74282ef4d7cf4c60aa7533
Minh Nghĩa <45595926+nghiaphamln@users.noreply.github.com>
2022-12-22 21:03:30 +0700

    upd(server): refactor code

--- backend-expressjs/app.js
+++ backend-expressjs/app.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
 const express = require('express');
-const database = require('./config/database.config');
+const {ConnectDatabase} = require('./config/database.config');
 const HandleError = require('./middleware/handleError.middleware');
 const NotFound = require('./middleware/notFound.middleware');
 const Log = require('./middleware/log.middleware');
 
 const app = express();
-database.ConnectDatabase().then(r => r);
+ConnectDatabase().then(r => r);
 
 app.set('trust proxy', true)
 app.use(Log);
